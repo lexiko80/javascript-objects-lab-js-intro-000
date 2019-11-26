ccf15725e8466c6901088993ac31e947a32decfd
Alexis Kotzambasis <lexiko80@gmail.com>
2019-11-26 04:31:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var destructivelyUpdateObjectWithKeyAndValue = (object, key, value)=>{
 }
 
 var deleteFromObjectByKey = (object, key)=>{
-  var newObj = Object.assign({}, obj)
+  var newObj = Object.assign({}, object)
   delete newObj[key]
 
   return newObj
