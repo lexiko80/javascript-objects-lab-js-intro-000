86b44734df18a98c73ac90ed09cce490ee29b6b6
Alexis Kotzambasis <lexiko80@gmail.com>
2019-11-26 04:11:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function updateObjectsWithKeyAndValue(object, key, value){
-  newObj = Object.assign({}, object, {[key]:value})
+  newObj = Object.assign({}, object, {key:value})
 
   return newObj
 }
